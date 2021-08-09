f566beeeaa55b0df425a855b970e98e28aaf6a12
shushu <minsoo59@naver.com>
2021-08-09 22:12:25 +0900

    add route get

--- servers/routes/homeRouter.js
+++ servers/routes/homeRouter.js
@@ -1,18 +1,19 @@
 import express from "express";
 import { home, main } from "../controllers/homeController.js";
 import {
-  getJoin,
+  getEditProfile,
   postEditProfile,
   postJoin,
   postLogin,
+  getLogin,
 } from "../controllers/userController.js";
 
 const homeRouter = express.Router();
 
 homeRouter.get("/", home);
 homeRouter.route("/join").post(postJoin);
-homeRouter.route("/login").post(postLogin);
-homeRouter.route("/profile").post(postEditProfile);
+homeRouter.route("/login").get(getLogin).post(postLogin);
+homeRouter.route("/profile").get(getEditProfile).post(postEditProfile);
 homeRouter.get("/main", main);
 
 export default homeRouter;
